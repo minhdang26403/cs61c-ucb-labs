94ec9a8ec3ae3d1af912652bab6031d8f9dd154e
Dang Truong <truongminhdang26403@gmail.com>
2022-12-24 17:32:25 -0500

    Completed Lab 2.

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,10 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+  uint16_t x = *reg;
+  uint16_t xor = (x & 0x1) ^ ((x >> 2) & 0x1) ^ ((x >> 3) & 0x1) ^ ((x >> 5) & 0x1);
+  x >>= 1; // shift register to the right
+  x &= ~(1 << 15); // clear the left most bit
+  *reg = x | (xor << 15); // set the left most bit to the value of xor 
 }
 
