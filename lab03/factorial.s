7957e4fa17396ee24764c35d5a34ccc75bbfd94f
Dang Truong <truongminhdang26403@gmail.com>
2022-12-29 18:25:06 -0500

    Completed exercise 3.

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,13 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    add t0, x0, a0
+    addi t1, x0, 1
+loop:
+    beq t0, x0, exit
+    mul t1, t1, t0
+    addi t0, t0, -1
+    jal x0, loop
+exit:
+    add a0, x0, t1
+    jr ra
